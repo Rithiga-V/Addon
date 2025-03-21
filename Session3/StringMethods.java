8edd4b29616344f0df28afcc085e442e0864aed4
Rithiga-V <163977667+Rithiga-V@users.noreply.github.com>
2025-03-20 20:33:25 -0700

    Session3

---
+++ Session3/StringMethods.java
@@ -0,0 +1,10 @@
+package Session3;
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		String s1 = "Java Program";
+		//1.length
+		System.out.println(s1.length());
+	}
+}
